6161eb8292337a4855e31a08dbfdb4c026495b50
Rahul Patel <mp902785@gmail.com>
2025-02-07 17:04:48 +0530

    Updated layout for activity cards in repository analytics

--- components/analytics/repository-analytics.tsx
+++ components/analytics/repository-analytics.tsx
@@ -509,7 +509,7 @@ export function RepositoryAnalytics({ repository, accessToken }: RepositoryAnaly
             </CardContent>
           </Card>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="grid grid-cols-1 gap-4">
             {/* Issues Activity */}
             <Card>
               <CardHeader>
